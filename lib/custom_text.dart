89c76f731f1eca97682c3363aa0afff165ed053f
Kabo <20254485+kaboc@users.noreply.github.com>
2023-02-17 11:02:57 +0900

    Expose base definition class and typedefs of builders of SelectiveDefinition and SpanDefinition

--- lib/custom_text.dart
+++ lib/custom_text.dart
@@ -1,5 +1,6 @@
 export 'package:text_parser/text_parser.dart';
 
+export 'src/definition_base.dart';
 export 'src/definitions.dart';
 export 'src/gesture_details.dart';
 export 'src/matchers.dart';
--- lib/src/definition_base.dart
+++ lib/src/definition_base.dart
@@ -1,17 +1,28 @@
-import 'package:meta/meta.dart';
-
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 
-import 'package:text_parser/text_parser.dart';
+import 'package:meta/meta.dart';
+import 'package:text_parser/text_parser.dart' show TextMatcher;
 
 import 'gesture_details.dart';
+import 'text.dart';
+import 'text_editing_controller.dart';
 
+/// The signature for a callback to select a string to display.
 typedef LabelSelector = String Function(List<String?>);
+
+/// The signature for a callback to select a string to be passed
+/// to gesture callbacks.
 typedef TapSelector = String Function(List<String?>);
+
+/// The signature for a callback to return an [InlineSpan] to
+/// be used as part of the text to display.
 typedef SpanBuilder = InlineSpan Function(String, List<String?>);
 
+/// The base class for definitions of rules for parsing, appearance
+/// and actions for [CustomText] and [CustomTextEditingController].
 @immutable
+@sealed
 abstract class Definition {
   const Definition({
     required this.matcher,
