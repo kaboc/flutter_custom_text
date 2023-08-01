a09f2a7555090b2a1933d43ce6d3ed9a36999793
Kabo <20254485+kaboc@users.noreply.github.com>
2023-08-01 12:44:28 +0900

    Add a test

--- test/composition_test.dart
+++ test/composition_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:custom_text/custom_text.dart';
+
 import 'utils.dart';
 import 'widgets.dart';
 
@@ -57,6 +59,41 @@ void main() {
         expect((spans[2] as TextSpan).text, equals('foo@example.com'));
         expect((spans[3] as TextSpan).text, equals(', Tel: '));
         expect((spans[4] as TextSpan).text, equals('012-3456-7890'));
+      },
+    );
+
+    testWidgets(
+      "Multiple definitions with matcher of same type don't get mixed up",
+      (tester) async {
+        await tester.pumpWidget(
+          const CustomTextWidget(
+            'Pattern 2 Pattern 3 foo@example.com Pattern 1',
+            definitions: [
+              TextDefinition(
+                matcher: EmailMatcher(),
+                matchStyle: TextStyle(color: Color(0xFF111111)),
+              ),
+              TextDefinition(
+                matcher: PatternMatcher('Pattern 1'),
+                matchStyle: TextStyle(color: Color(0xFF222222)),
+              ),
+              TextDefinition(
+                matcher: PatternMatcher('Pattern 2'),
+                matchStyle: TextStyle(color: Color(0xFF333333)),
+              ),
+            ],
+          ),
+        );
+        await tester.pump();
+
+        final spans = getSpans();
+        expect(spans, hasLength(5));
+        expect(spans[0].toPlainText(), 'Pattern 2');
+        expect(spans[0].style?.color, const Color(0xFF333333));
+        expect(spans[2].toPlainText(), 'foo@example.com');
+        expect(spans[2].style?.color, const Color(0xFF111111));
+        expect(spans[4].toPlainText(), 'Pattern 1');
+        expect(spans[4].style?.color, const Color(0xFF222222));
       },
     );
   });
